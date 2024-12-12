bd99de0cc39bc09752fa628d47aeb0b3e54a2625
ratty-lab <ratty06123@gmail.com>
2024-12-12 10:28:15 +0530

    test

---
+++ server.js
@@ -0,0 +1,43 @@
+
+
+import express from "express";
+import http from 'http';
+import { Server} from 'socket.io';
+import dotenv from 'dotenv'
+
+const app=express();
+const server=http.createServer(app);
+app.set('view engine', 'ejs');
+
+
+const io=new Server(server)
+const users={};
+
+
+app.get("/",(req,res)=>{
+    res.render('index.ejs');
+})
+io.on('connection',(socket)=>{
+    socket.on('adduser',(username)=>{
+        
+        users[socket.id]= username;
+        
+        console.log(users);
+        
+        
+        
+    })
+    socket.on('user-message',(message)=>{
+        const socketId=socket.id;
+        const username = users[socketId]?.username;
+        console.log('message by:',username);
+        io.emit('message',`${username} : ${message}`);
+        
+        
+    })
+    
+})
+server.listen( process.env.PORT || 5000,()=>{
+    console.log('server is running at port 3000');
+    
+})
